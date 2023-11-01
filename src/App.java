1388aeb82b7d84a991f439d81cac9be38425fba0
brandonrbreeze <bbreeze@ucsd.edu>
2023-10-31 17:26:30 -0700

    Basic app

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -15,78 +14,53 @@ import javafx.geometry.Insets;
 import javafx.scene.text.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
-class Contact extends HBox {
-    private Label index;
-    private Label contactImage;
-    private ImageView view;
-    private Image image = null;
-    private Texts texts;
-    private Button editButton;
-    private Button deleteButton;
+class Footer extends HBox {
 
-    class Texts extends VBox {
-        private TextField name;
-        private TextField number;
-        private TextField email;
-}
-
-class ContactList extends VBox {
+    Footer() {
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        this.setSpacing(15);
+    }
 
 }
 
 class Header extends HBox {
+
+    Header() {
+        this.setPrefSize(500, 60); // Size of the header
+        this.setStyle("-fx-background-color: #F0F8FF;");
+
+        Text titleText = new Text("PantryPal"); // Text of the Header
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        this.getChildren().add(titleText);
+        this.setAlignment(Pos.CENTER); // Align the text to the Center
+    }
 }
 
-class Footer extends HBox {
-    private Button addButton;
-    private Button saveButton;
-    private Button sortButton;
-}
+class AppFrame extends BorderPane{
 
-
-class AppFrame extends BorderPane {
-    private Header header;
-    private Footer footer;
-    private ContactList contactList;
-    private ScrollPane scrollPane;
-    //private ImageUploaderApp imageUploader;
-
-    private Button addButton;
-    private Button saveButton;
-    private Button sortButton;
-
-    AppFrame() {
-        // Initialise the header Object
-        header = new Header();
-
-        // Create a ContactList Object to hold the contacts
-        contactList = new ContactList();
-        
-        // Initialise the Footer Object
-        footer = new Footer();
-
-        scrollPane = new ScrollPane(contactList);
-        scrollPane.setFitToHeight(true);
-        scrollPane.setFitToWidth(true);
-
-        // Add header to the top of the BorderPane
-        this.setTop(header);
-        // Add scroller to the centre of the BorderPane
-        this.setCenter(scrollPane);
-        // Add footer to the bottom of the BorderPane
-        this.setBottom(footer);
-
-    }
-
-    }
 }
 
 public class App extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        // Setting the Layout of the Window- Should contain a Header, Footer and the TaskList
+        AppFrame root = new AppFrame();
+
+        // Set the title of the app
+        primaryStage.setTitle("PantryPal");
+        // Create scene of mentioned size with the border pane
+        primaryStage.setScene(new Scene(root, 500, 600));
+        // Make window non-resizable
+        primaryStage.setResizable(false);
+        // Show the app
+        primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }

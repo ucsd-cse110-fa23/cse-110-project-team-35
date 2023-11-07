9500ab2c59bb84be650208a303c30fd6f4ffbf43
brandonrbreeze <bbreeze@ucsd.edu>
2023-11-06 21:34:26 -0800

    Made changes to List Function

--- app/src/main/java/cse/project/team/Model.java
+++ app/src/main/java/cse/project/team/Model.java
@@ -17,27 +17,21 @@ public class Model {
         return recipeList;
     }
 
-    public void addToList(String title){
-        recipeList.add(title);
-    }
-
     public HashMap<String, String> getData() {
         return data;
     }
 
     public void addData(String title, String text) {
         data.put(title, text);
+        recipeList.add(title);
     }
 
     public void deleteData(String title) {
         data.remove(title);
+        recipeList.remove(title);
     }
 
     public String getDetails(String title) {
         return data.get(title);
-    }
-
-    public void deleteFromList(String currTitle) {
-        recipeList.remove(currTitle);
     }
 }

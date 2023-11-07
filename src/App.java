b502470ba9096a9f559bccff6045ab4711b1dfbc
Lea <114596129+leuska@users.noreply.github.com>
2023-11-06 16:21:50 -0800

    Testing GitHub Actions

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class App extends Application {
     
     public static void main(String[] args) {

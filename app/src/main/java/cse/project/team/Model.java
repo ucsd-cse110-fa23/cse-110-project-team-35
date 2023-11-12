1ec288aa81db4990784d4c59e7a517465fba187a
yangangx <114597727+yangangx@users.noreply.github.com>
2023-11-11 22:00:46 -0800

    src

--- app/src/main/java/cse/project/team/Model.java
+++ app/src/main/java/cse/project/team/Model.java
@@ -32,9 +32,11 @@ public class Model {
         generation = new genAPI();
         
 
-        String uri = "mongodb+srv://yax016:@cluster0.tqvgogm.mongodb.net/?retryWrites=true&w=majority";
+        //String uri = "mongodb+srv://yax016:@cluster0.tqvgogm.mongodb.net/?retryWrites=true&w=majority";
 
-        MongoClient mongoClient = MongoClients.create(uri);
+        //MongoClient mongoClient = MongoClients.create(uri);
+
+        MongoClient mongoClient = MongoClients.create("mongodb+srv://yig017:@cluster0.cfigpzh.mongodb.net/?retryWrites=true&w=majority");
         MongoDatabase db = mongoClient.getDatabase("cse110_project");
         this.recipeCollection = db.getCollection("recipes");
     }

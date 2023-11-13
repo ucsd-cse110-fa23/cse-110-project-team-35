253094c55ab3e0f4eb97fd2bea415dacac4b98ec
brandonrbreeze <bbreeze@ucsd.edu>
2023-11-12 18:38:51 -0800

    changes to tests

--- app/src/test/java/cse/project/team/AppTest.java
+++ app/src/test/java/cse/project/team/AppTest.java
@@ -1,5 +1,7 @@
 package cse.project.team;
 
+import cse.project.team.server.RequestHandler;
+import cse.project.team.server.genAPI;
 import cse.project.team.server.server;
 import org.junit.jupiter.api.Test;
 import org.junit.AfterClass;
@@ -19,24 +21,7 @@ class AppTest {
     String m_details = "Get potatoes. Mash. Done.";
     String p_title = "Pancakes";
     String p_details = "Get cake. Get pan. Put cake in pan. Done.";
-    private static server server;
-
-    @BeforeClass
-    public static void setUp() {
-        try {
-            server = new server();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
-
-    @AfterClass
-    public static void stopServer() {
-        server.stop();
-        
-    }
+    RequestHandler rh;
     
     private static final String SERVER_URL = "http://localhost:8100/";
 
@@ -49,64 +34,44 @@ class AppTest {
     public void testHandlePost() throws Exception{
         String expectedResponse_title = "Mashed potat";
         String expectedResponse_detail = m_details;
-        Model model = new Model();
-        String response_post = model.performRequest("POST", m_title, m_details, "m");
-        String response = model.performRequest("GET", m_title, m_details, null);
-        String response_detail = model.performRequest("GET", null, null, response);
-        assertEquals(expectedResponse_title, response);
-        assertEquals(expectedResponse_detail, response_detail);
+        RequestHandler rh = new RequestHandler(new genAPI());
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
     }
 
     @Test
     public void testHandleGet() throws IOException {
-        
-
-        String expectedResponse = "";
-        Model model = new Model();
-        // Perform the GET request
-        // String responseDelete = model.performRequest("DELETE", null, null, "Mashed
-        // potat");
-        String response = model.performRequest("GET", m_title, m_details, null);
-
-        assertEquals(expectedResponse, response);
+        String expectedResponse_title = "Mashed potat";
+        String expectedResponse_detail = m_details;
+        RequestHandler rh = new RequestHandler(new genAPI());
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
     }
 
     @Test
     public void testHandlePUTandGET() throws IOException {
-
         String expectedResponse_title = "Mashed potat";
         String expectedResponse_detail = m_details;
-        String putdone = "Did Something";
-        Model model = new Model();
-        String response_put = model.performRequest("PUT", m_title, m_details, null);
-        String response = model.performRequest("GET", m_title, m_details, null);
-
-        assertEquals(response_put, putdone);
-        assertEquals(expectedResponse_title, response);
-
+        RequestHandler rh = new RequestHandler(new genAPI());
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
     }
 
     @Test
     public void testHandlePUT() throws IOException {
         String expectedResponse_title = "Mashed potat";
-        String putdone = "Did Something";
         String expectedResponse_detail = m_details;
-        Model model = new Model();
-        String response_put = model.performRequest("PUT", m_title, m_details, null);
-        assertEquals(putdone, response_put);
-
+        RequestHandler rh = new RequestHandler(new genAPI());
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
     }
 
     @Test
     public void testHandlDELETE() throws Exception {
         String expectedResponse_title = "Mashed potat";
         String expectedResponse_detail = m_details;
-        Model model = new Model();
-        String response_post = model.performRequest("PUT", m_title, m_details, null);
-        String response = model.performRequest("GET", m_title, m_details, null);
-        String response_detail = model.performRequest("GET", null, null, response);
-        String responseDelete = model.performRequest("DELETE", null, null, response);
-        assertEquals(expectedResponse_title, response);
-        assertEquals(expectedResponse_detail, response_detail);
+        RequestHandler rh = new RequestHandler(new genAPI());
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
     }
 }

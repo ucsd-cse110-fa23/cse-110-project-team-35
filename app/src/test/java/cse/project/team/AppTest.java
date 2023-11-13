4ebdbb9328edf81ba2dfd6b63b223fd98fda20f8
yangangx <114597727+yangangx@users.noreply.github.com>
2023-11-12 22:15:20 -0800

    test

--- app/src/test/java/cse/project/team/AppTest.java
+++ app/src/test/java/cse/project/team/AppTest.java
@@ -39,18 +39,11 @@ class AppTest {
     @Test
     public void testHandleViewDetail() throws Exception{
         String expectedResponse_detail = "Get potatoes. Mash. Done.";
-        String expectedResponse_detailWithEmptyList = "";
         RequestHandler rh = new RequestHandler(new genAPI());
-        String list = rh.getRecList();
-        if (list.equals(""))
-        {
-            assertEquals(expectedResponse_detail, expectedResponse_detailWithEmptyList);
-        }
-        else
-        {
-            String detail = rh.getRecDetail(m_title);
-            assertEquals(expectedResponse_detail, detail);
-        }
+        String detail = rh.getRecDetail(m_title);
+        assertEquals(expectedResponse_detail, detail);
+
+
         
     }
 

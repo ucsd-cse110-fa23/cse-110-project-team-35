1401ba69df79bc2c22a505720c2d608f10ae2439
leawinr <114596129+leawinr@users.noreply.github.com>
2023-11-12 22:47:23 -0800

    Added tests for Delete

--- app/src/test/java/cse/project/team/AppTest.java
+++ app/src/test/java/cse/project/team/AppTest.java
@@ -2,7 +2,10 @@ package cse.project.team;
 
 import cse.project.team.server.RequestHandler;
 import cse.project.team.server.genAPI;
+import cse.project.team.server.genMock;
 import cse.project.team.server.server;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -24,21 +27,20 @@ class AppTest {
     String p_title = "Pancakes";
     String p_details = "Get cake. Get pan. Put cake in pan. Done.";
     private static final String SERVER_URL = "http://localhost:8100/";
-    RequestHandler handler = new RequestHandler(new genAPI());
+    RequestHandler handler = new RequestHandler(new genMock());
 
     @BeforeEach
-    public void clearDatabase() {;
+    public void clearDatabase() {
         handler.clear();
     }
 
-    // the server should be already running
     @Test
-    public void testCheck() throws Exception{
-        assertEquals(1+1, 2);
+    public void testCheck() throws Exception {
+        assertEquals(1 + 1, 2);
     }
 
     @Test
-    public void testHandleEditRecipe() throws Exception{
+    public void testHandleEditRecipe() throws Exception {
         String expectedResponse_detail = "Get potatoes. Mash. Done.";
         handler.doPost(m_title, expectedResponse_detail);
         String detail = handler.getRecDetail(m_title);
@@ -49,24 +51,43 @@ class AppTest {
     public void testEmptyList() throws Exception{
         String list = handler.getRecList();
         String expect = "";
-        assertEquals(expect,list);
+        assertEquals(expect, list);
     }
 
     @Test
-    public void testFullList() throws Exception{
+    public void testFullList() throws Exception {
         handler.doPost(m_title, m_details);
         handler.doPost(p_title, p_details);
         String list = handler.getRecList();
-        String expect = m_title+"*"+p_title;
-        assertEquals(expect,list);
+        String expect = m_title + "*" + p_title;
+        assertEquals(expect, list);
         handler.clear();
     }
 
     @Test
-    public void testHandleViewDetail2() throws Exception{
+    public void testHandleViewDetail2() throws Exception {
         String expectedResponse_detail = "Get potatoes. Mash. Done.";
         handler.doPost(m_title, m_details);
         String detail = handler.getRecDetail(m_title);
         assertEquals(expectedResponse_detail, detail); 
+    }
+
+    @Test
+    public void testHandleDELETE() throws Exception {
+        deleteGiven(m_title, m_details);
+        deleteWhen(m_title);
+        deleteThen(m_title, "Does not exist");
+    }
+
+    public void deleteGiven(String title, String details) {
+        handler.doPost(title, details);
+    }
+
+    public void deleteWhen(String title) {
+        handler.doDelete(title);
+    }
+
+    public void deleteThen(String title, String response) {
+        assertEquals(response, handler.getRecDetail(title));
     }
 }
